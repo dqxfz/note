eb17b7459047d073a7ccbf6f1005312146a93642
dqxfz <1612487591@qq.com>
2020-06-17 23:16:17 +0800

    test

--- portal/src/test/java/site/dqxfz/portal/Spring5ApplicationTest02.java
+++ portal/src/test/java/site/dqxfz/portal/Spring5ApplicationTest02.java
@@ -1,5 +1,7 @@
 package site.dqxfz.portal;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,16 +20,20 @@ import java.util.concurrent.TimeUnit;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = KafkaConfig.class)
 public class Spring5ApplicationTest02 {
-
+    private final Logger logger = LogManager.getLogger(this.getClass());
     @Autowired
     private KafkaTemplate<Integer, String> template;
 
     @Test
-    public void testSimple() throws Exception {
+    public void test01() throws Exception {
 //        ConsumerFactory.Listener
-//        template.send("annotated1", 0, "foo");
-//        template.flush();
-        TimeUnit.SECONDS.sleep(100);
+        template.send("test", 0, "{'time':'2020.06.17 19:56:03.488','level':'INFO','class':'site.dqxfz.portal.Spring5ApplicationTest02','line':'36','method':'test02','content':'hello'}");
+        template.flush();
+//        TimeUnit.SECONDS.sleep(100);
+    }
+    @Test
+    public void test02(){
+        logger.info("hello");
     }
 
 }
